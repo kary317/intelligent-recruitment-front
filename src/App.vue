0d615c3b74b948b25f61f5dea9eb0aeb17b70ed1
kary <kary.sun@outlook.com>
2026-07-22 10:55:45 +0800

    登录页面实现

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  <el-button icon="Plus" type="primary">新增</el-button>
+  <router-view />
 </template>
 
 <style scoped></style>
